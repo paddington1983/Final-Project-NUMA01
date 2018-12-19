ae1b2ed5c39cc20e130d9a57b7ac35d5e0ba6906
Ramon van de Scheur <ramon_van_de_scheur@hotmail.com>
2018-12-19 15:48:11 +0100

    Initial commit, the 2 files that we will be using

---
+++ fractals.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 19 15:40:25 2018
+
+@author: Ramon
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
---
+++ fractals_test.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 19 15:41:13 2018
+
+@author: Ramon
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+

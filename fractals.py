b376e3af1d6832fe45a62e3836cd7b368fe011f6
Ramon van de Scheur <ramon_van_de_scheur@hotmail.com>
2018-12-20 15:08:57 +0100

    Made a slight change to the field names and added a stub for findZeroPosition, removed hello world

--- fractals.py
+++ fractals.py
@@ -7,10 +7,12 @@ Created on Wed Dec 19 15:40:25 2018
 import matplotlib.pyplot as plt
 import numpy as np
 
-print('hello, world!')
-
 class fractal2D:
-	def __init__(self, function,derivativematrix):
-		self.function=function
-		self.derivativematrix=derivativematrix
-		self.zeroes=[]
+    def __init__(self, functionVector, derivativeMatrix):
+        self.functionVector = functionVector
+        self.derivativeMatrix = derivativeMatrix
+        self.zeroes = []
+    def findZeroPosition(self, guess):
+        """Finds the position of a zero using Newton's Method, starting from guess, also returns how many iterations it took"""
+        
+        return guess, 0
